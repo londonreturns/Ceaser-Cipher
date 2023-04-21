cc1e6df6591bfc25624bf85d4e78dd9ff1a347bb
londonreturns <subedioscar.47@gmail.com>
2023-04-21 08:56:48 +0545

    Continue added

--- ceaser.py
+++ ceaser.py
@@ -5,38 +5,74 @@ this program encrypts and decrypts text using Ceaser Cipher
 class NotSring(Exception):
     pass
 
-
 class NotWholeNumber(Exception):
     pass
 
+class NotYesOrNo(Exception):
+    pass
+
+def want_to_continue():
+    while True:
+        try:
+            again = input("Do you want to continue (y)es or (n)o: ").lower()
+            if not (again == "y" or again == "n"):
+                raise NotYesOrNo
+        except NotYesOrNo:
+            print("Please enter only yes or no")
+        finally:
+            if again == "y":
+                return True
+            else:
+                return False
+                        
+def encrypt(text, shift_by):
+    print("encrypt", text, shift_by)
+    
+def decrypt(text, shift_by):
+    print("decrypt", text, shift_by)
+
+def enter_shift():
+    while True:
+        try:
+            shift_num = int(input("Enter the shift number: "))
+            if shift_num < 0:
+                raise NotWholeNumber
+        except NotWholeNumber:
+                print("Please enter a number greater than zero")
+                continue
+        except:
+            print("Invalid Shift number")
+            continue
+        finally:
+            return shift_num
+                        
 def enter_message():
     while True:
         try:
-            choice = input("Would you like to encrypt (e) or decrypt (d)")
-            if not (choice.lower() == "e" or choice.lower() == "d"):
+            choice = input("Would you like to encrypt (e) or decrypt (d): ").lower()
+            if not (choice == "e" or choice == "d"):
                 raise NotSring
             else:
-                while True:
-                    try:
-                        shift = int(input("Enter the shift number"))
-                        if shift < 0:
-                            raise NotWholeNumber
-                    except NotWholeNumber:
-                          print("Please enter a number greater than zero")
-                    except:
-                        print("Invalid Mode")      
+                shift = enter_shift()
+                if choice == "e":
+                    text_input = input("What message would you like to encrypt: ")
+                    encrypt(text_input, shift)
+                else:
+                    text_input = input("What message would you like to decrypt: ")
+                    decrypt(text_input, shift)
         except NotSring:
             print("Invalid Mode")
             continue
-
-        
-
+        if want_to_continue():
+            enter_message()
+        else:
+            break
 
 def main():
     """this is the main function of the program"""
     print("""
-          Welcome to the Ceaser Cipher
-          This program encrypts and decrypts text with the Ceaser Cipher. 
+Welcome to the Ceaser Cipher
+This program encrypts and decrypts text with the Ceaser Cipher. 
           """)
     enter_message()
     
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+
+
+
+def main():
+    """this is the main function of the program"""
