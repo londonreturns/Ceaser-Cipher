d87f02bc307f503da354db1b8dcc1f0b5f580d72
londonreturns <subedioscar.47@gmail.com>
2023-04-25 09:49:58 +0545

    fin

--- ceaser.py
+++ ceaser.py
@@ -9,12 +9,9 @@ Ouput are given in Yellow.
 """
 
 from os import startfile
+from os.path import exists
 from colorama import Fore
 from colorama import init as colorama_init
-
-class NotRequiredString(Exception):
-    """NotRequiredString: is raised when a string contains characters other than alphabets"""
-
 
 class NotWholeNumber(Exception):
     """NotWholeNumber: is raised when a string is nor a whole number"""
@@ -45,24 +42,25 @@ def want_to_continue():
 
 def encrypt_decrypt_key_generator(encrypt_or_decrypt, shift_number):
     """This function generates the key to encrypt an decrypt"""
-    all_letters = 'abcdefghijklmnopqrstuvwxyz'
+    all_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     key_dictionary = {}
     j = 0
     if shift_number > 26:
         shift_number %= 26
     if encrypt_or_decrypt == 'e':
-        for i in range(len(all_letters)):
-            if shift_number + i < 26:
-                key_dictionary[all_letters[i]] = all_letters[i + shift_number]
-            else:
-                key_dictionary[all_letters[i]] = all_letters[j]
-                j += 1
+        if encrypt_or_decrypt == 'e':
+            for i, letter in enumerate(all_letters):
+                if shift_number + i < 26:
+                    key_dictionary[letter] = all_letters[i + shift_number]
+                else:
+                    key_dictionary[letter] = all_letters[j]
+                    j += 1
     else:
-        for i in range(len(all_letters)):
+        for i, letter in enumerate(all_letters):
             if shift_number + i < 26:
-                key_dictionary[all_letters[i + shift_number]] = all_letters[i]
+                key_dictionary[all_letters[i + shift_number]] = letter
             else:
-                key_dictionary[all_letters[j]] = all_letters[i]
+                key_dictionary[all_letters[j]] = letter
                 j += 1
     return key_dictionary
 
@@ -98,6 +96,99 @@ def decrypt(text, shift_by):
     return temp
 
 
+def is_file(file_name):
+    """This function checks if file exists or not and returns Boolean value"""
+    return exists(file_name)
+
+
+def enter_message():
+    """This is the enter_message function.
+    Prompts user to input encrypt or decrypt.
+    Prompts user to enter message."""
+    while True:
+        choice_ed = input(
+            f'{Fore.CYAN}\nWould you like to encrypt (e) or decrypt (d): {Fore.RESET}').lower()
+        if not(choice_ed == 'e' or choice_ed == 'd'):
+            print(f'{Fore.RED}Invalid Mode{Fore.RESET}')
+        else:
+            if choice_ed == 'e':
+                text_input = input(
+                    f'{Fore.CYAN}\nWhat message would you like to encrypt: {Fore.RESET}').upper()
+            else:
+                text_input = input(
+                    f'{Fore.CYAN}\nWhat message would you like to decrypt: {Fore.RESET}').upper()
+            return choice_ed, text_input
+
+
+def message_or_file():
+    """This is the message_or_file function.
+    Prompts user to input encrypt or decrypt.
+    Prompts user to enter message.
+    Prompts user to enter filename"""
+    while True:
+        choice_ed = input(
+            f'{Fore.CYAN}\nWould you like to encrypt (e) or decrypt (d): {Fore.RESET}').lower()
+        if not (choice_ed =='e' or choice_ed =='d'):
+            print(f'{Fore.RED}Invalid Mode{Fore.RESET}')
+            continue
+        else:
+            while True:
+                choice_cf = input(
+                    f'{Fore.CYAN}\nWould you like enter in console (c) or file (f): {Fore.RESET}'
+                    ).lower()
+                if not (choice_cf =='c' or choice_cf == 'f'):
+                    print(f'{Fore.RED}Invalid Mode{Fore.RESET}')
+                    continue
+                else:
+                    if choice_cf == 'c' and choice_ed == 'e':
+                        text_input = input(
+                            f'{Fore.CYAN}\nWhat message would you like to encrypt: {Fore.RESET}'
+                            ).upper()
+                        file_name = None
+                    elif choice_cf == 'c' and choice_ed == 'd':
+                        text_input = input(
+                            f'{Fore.CYAN}\nWhat message would you like to encrypt: {Fore.RESET}'
+                            ).upper()
+                        file_name = None
+                    else:
+                        file_name = input(
+                            f'{Fore.CYAN}\nWhat message would you like to decrypt: {Fore.RESET}'
+                            ).upper()
+                        text_input = None
+                return choice_ed, text_input, file_name
+
+
+def process_file(file_name, mode, shift_number):
+    """This is process_file function.
+    It takes in file_name, mode, shift_number and makes list of encrypted string.
+    """
+    with open(file_name, 'r', encoding = 'utf-8') as file1:
+        temp_list = []
+        if mode == 'e':
+            for line in file1:
+                for character in line:
+                    temp_character = encrypt(character.upper(), shift_number)
+                    temp_list.append(temp_character)
+        else:
+            for line in file1:
+                for character in line:
+                    temp_character = decrypt(character.upper(), shift_number)
+                    temp_list.append(temp_character)
+        return temp_list
+
+
+def write_messages(all_characters):
+    """This is write_message function.
+        It takes a list and writes in an external file
+    """
+    while True:
+        write_file = input(f'{Fore.CYAN}\nOutput written to: {Fore.RESET}')
+        with open(write_file, 'w', encoding = 'utf-8') as file2:
+            for character in all_characters:
+                file2.write(character)
+            break
+
+
 def enter_shift():
     """This is the enter_shift function.
     Prompts user to enter shift number for the cipher.
@@ -121,62 +212,6 @@ def enter_shift():
     return shift_num  # shift_number is returned
 
 
-def enter_message():
-    """This is the enter_message function.
-    Prompts user to input encrypt or decrypt.
-    Prompts user to enter message."""
-    is_continue = True
-    while is_continue:
-        try:
-            # prompt user for choice
-            choice_ed = input(
-                f'{Fore.CYAN}\nWould you like to encrypt (e) or decrypt (d): {Fore.RESET}').lower()
-            choice_cf = input(
-                f'{Fore.CYAN}\nWould you like enter in console (c) or file (f): {Fore.RESET}').lower()
-            if not ((choice_ed == 'e' or choice_ed == 'd') and
-                    (choice_cf == 'c' or choice_cf == 'f')):
-                raise NotRequiredString
-            else:
-                shift = enter_shift()  # calls enter_shift function
-                if choice_ed == 'e' and choice_cf == 'c':
-                    # prompts user for text input
-                    text_input = input(
-                        f'{Fore.CYAN}\nWhat message would you like to encrypt: {Fore.RESET}').lower()
-                    print(Fore.YELLOW + encrypt(text_input, shift) + Fore.RESET)
-                elif choice_ed =='d' and choice_cf == 'c':  # calls decrypt function if choice is d
-                    # prompts user for text input
-                    text_input = input(
-                        f'{Fore.CYAN}\nWhat message would you like to decrypt: {Fore.RESET}').lower()
-                    print(Fore.YELLOW + decrypt(text_input, shift) + Fore.RESET)
-                else:
-                    read_file = input(f'{Fore.CYAN}\nEnter the file name: {Fore.RESET}')
-                    with open(read_file, 'r', encoding = 'utf-8') as file1:
-                        all_text = ''
-                        if choice_ed == 'e':
-                            for line in file1:
-                                current_line = encrypt(line.lower(), shift)
-                                all_text += current_line
-                        else:
-                            for line in file1:
-                                current_line = decrypt(line.lower(), shift)
-                                all_text += current_line
-                    write_file = input(f'{Fore.CYAN}\nOutput written to: {Fore.RESET}')
-                    with open(write_file, 'w', encoding = 'utf-8') as file2:
-                        file2.writelines(all_text)
-                    startfile(write_file)
-        # exception is triggered if choice is not (y or n)
-        except NotRequiredString:
-            print(f'{Fore.RED}Invalid Mode{Fore.RESET}')
-            continue
-        except IOError:
-            print(f'{Fore.RED}File not found{Fore.RESET}')
-        is_continue = want_to_continue()  # call want_to_function
-        if not is_continue:
-            print(f'''{Fore.GREEN}
-Thank you for using this program, goodbye!''')
-            break
-
-
 def welcome():
     """This is the Welcome function to this program."""
     print(f'''{Fore.GREEN}
@@ -186,9 +221,35 @@ This program encrypts and decrypts text with the Ceaser Cipher.''')
 
 def main():
     """This is the main function of the program"""
-    colorama_init(autoreset=True)
     welcome()  # calls welcome function
-    enter_message()  # calls enter message function
+    still_continue = True
+    run_file = False
+    while still_continue:
+        colorama_init(autoreset=True)
+        if run_file:
+            choice, message, file1 = message_or_file()
+        else:
+            choice, message = enter_message()  # calls enter message function
+            file1 = None
+        shift_number = enter_shift()
+        if file1 is None:
+            if choice == 'e':
+                print(encrypt(message, shift_number))
+            else:
+                print(decrypt(message, shift_number))
+        else:
+            while not is_file(file1):
+                print(f'{Fore.RED}File not found{Fore.RESET}')
+                file1 = input(
+                    f'{Fore.CYAN}\nWhat message would you like to process: {Fore.RESET}').upper()
+            changed_text_list = process_file(file1, choice, shift_number)
+            write_messages(changed_text_list)
+            startfile(file1)
+        if still_continue:
+            run_file = True
+            still_continue = want_to_continue()
+            continue
+        print(f'{Fore.GREEN}Thank you for using my program{Fore.RESET}')
 
 
 main()  # calls main function
